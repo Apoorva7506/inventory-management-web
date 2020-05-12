4e76a7c9bb3d0acb5f6cf0014d7b507d6ad351b8
unknown <apoorvaflame@gmail.com>
2020-05-12 20:06:19 +0530

    All permissions, status codes and Black added

--- src/app1/admin.py
+++ src/app1/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-from .models import Product_Transaction, Products, Bill, Items
-
+admin.site.register(User)
 admin.site.register(Product_Transaction)
 admin.site.register(Products)
 admin.site.register(Bill)
